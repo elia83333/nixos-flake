0b88520625f2fda155e77a36802d8bbb5e41f99c
elia83333 <elia33337@outlook.com>
2025-09-29 21:12:54 +0200

    Automated Commit - Rebuild

--- flake.nix
+++ flake.nix
@@ -1,31 +1,38 @@
 # ╞═════════════════════════════╡ Super Cool Flake ╞═════════════════════════════╡
-
 {
   description = "Elia83333's Flake";
 
 # ╞═══════════════════════════════╡ Inputs ╞═════════════════════════════════╡
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    home-manager.url = "github:nix-community/home-manager";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-
-# ╞═════════════════╡ Hostname Declaration (iusenixbtw) ╞═══════════════════════╡
-    
-    nixosConfigurations.iusenixbtw = nixpkgs.lib.nixosSystem {
-
-# ╞═══════════════════════════════╡ Variables ╞═════════════════════════════════╡
-   
+# ╞═══════════════════════════════╡ Outputs ╞════════════════════════════════╡
+  outputs = { self, nixpkgs, home-manager, ... }: let
     system = "x86_64-linux";
-    system.stateVersion = "25.05";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+
+# ╞═════════════════╡ NixOS Host: iusenixbtw ╞═══════════════════════╡
+    nixosConfigurations.iusenixbtw = nixpkgs.lib.nixosSystem {
+      inherit system pkgs;
+
 # ╞═══════════════════════════════╡ Modules ╞═════════════════════════════════╡
-      
       modules = [
         ./config.nix
         ./packages.nix
-
       ];
+    };
+
+# ╞═════════════════╡ Home Manager Configuration ╞═════════════════════════╡
+    homeConfigurations.elia = home-manager.lib.homeManagerConfiguration {
+      inherit system pkgs;
+      modules = [ ./home/config.nix ];
+
+# ╞═══════════════════════════════╡ Home Manager State Version ╞═════════════╡
+      home.stateVersion = "25.05";
     };
   };
 }
+
