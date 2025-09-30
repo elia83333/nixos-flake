e09e1f9156c01d667c2fa06400027bc8aad3ef37
elia83333 <elia33337@outlook.com>
2025-09-30 15:10:29 +0200

    Automated Commit - HM Rebuild

--- home/default.nix
+++ home/default.nix
@@ -12,5 +12,26 @@ in {
   home.username = username;
   home.homeDirectory = "/home/" + username;
   home.stateVersion = flakeConfig.stateVersion;
+  
+  gtk = {
+    enable = true;
+    cursorTheme = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern";
+    };
+    iconTheme = {
+      package = pkgs.papirus-icon-theme;
+      name = "Papirus";
+    };
+    font = {
+      name = "Dejavu Sans Mono";
+    };
+  };
+  home.pointerCursor = {
+    gtk.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern";
+    size = 16;
+  };
 }
 
