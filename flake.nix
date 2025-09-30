2c1cae94a0565102c146c1e6994665c8ee66051c
elia83333 <elia33337@outlook.com>
2025-09-30 14:01:28 +0200

    Automated Commit - Rebuild

--- flake.nix
+++ flake.nix
@@ -1,48 +1,34 @@
-# ╞═════════════════════════════╡ Super Cool Flake ╞═════════════════════════════╡
 {
   description = "Elia83333's Flake";
 
-# ╞═══════════════════════════════╡ Inputs ╞═════════════════════════════════╡
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     home-manager.url = "github:nix-community/home-manager";
   };
 
-# ╞═══════════════════════════════╡ Outputs ╞════════════════════════════════╡
   outputs = { self, nixpkgs, home-manager, ... }: let
     system = "x86_64-linux";
   in {
-
-# ╞═════════════════╡ NixOS Host: iusenixbtw ╞═══════════════════════╡
     nixosConfigurations.iusenixbtw = nixpkgs.lib.nixosSystem {
       inherit system;
-
-# ╞═══════════════════════════════╡ pkgs ══════════════════════════════════╡
       pkgs = import nixpkgs {
         inherit system;
-        config = { allowUnfree = true; };
+        config.allowUnfree = true;
       };
-
-# ╞═══════════════════════════════╡ Modules ══════════════════════════════════╡
       modules = [
         ./config.nix
         ./packages.nix
       ];
     };
 
-# ╞═════════════════╡ Home Manager Configuration ╞═════════════════════════╡
     homeConfigurations.elia = home-manager.lib.homeManagerConfiguration {
-
-# ╞═══════════════════════════════╡ pkgs for HM ══════════════════════════════╡
       pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        config = { allowUnfree = true; };
+        inherit system;
+        config.allowUnfree = true;
       };
-
-# ╞═══════════════════════════════╡ Modules ══════════════════════════════════╡
       modules = [
+        ./home/default.nix
         ./fuckingFlakeStateVersion.nix
-        { home.stateVersion = "25.05"; }
       ];
     };
   };
--- home/default.nix
+++ home/default.nix
@@ -1,17 +1,15 @@
 { config, pkgs, inputs, ... }:
+
 let
-  flakeConfig = import ../fuckingFlakeStateVersion.nix;
   username = "elia";
-in
-{
+in {
   imports = [
-    #./configs/folder/file
+    # ./configs/folder/file
   ];
-  
+
   programs.home-manager.enable = true;
 
   home.username = username;
   home.homeDirectory = "/home/" + username;
-  home.stateVersion = flakeConfig.stateVersion;
-
 }
+
