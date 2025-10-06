8bb0c1ce2823a5d823543dbe8e9aeb1dcf7cede0
elia83333 <elia33337@outlook.com>
2025-10-06 21:18:21 +0200

    Automated Commit - HM Rebuild

--- home/configs/Xresources/default.nix
+++ home/configs/Xresources/default.nix
@@ -1,9 +1,5 @@
 { config, pkgs, ... }:
 
-let
-  # CHATGPT IF THIS DOESNT FUCKING WORK I WILL KILL YOU, EXTRACT UR ORGANS AND FEED EM TO UR FAMILY WHILE U WATCH. mightve been a bit too much prob gonna remove this commend late
-  xresourcesFile = builtins.toString ./ ./.Xresources;
-in
 {
-  home.file.".Xresources".source = xresourcesFile;
+  home.file.".Xresources".source = ./Xresources/.Xresources;
 }
