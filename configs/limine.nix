b2a5c78ce451923a13c1ee718f9c9bb69ae647df
elia83333 <elia33337@outlook.com>
2025-10-07 21:22:20 +0200

    Automated Commit - Rebuild

--- configs/limine.nix
+++ configs/limine.nix
@@ -8,8 +8,4 @@
   boot.loader.limine.style.interface.brandingColor = 7;
   boot.loader.limine.style.graphicalTerminal.palette = "1e1e2e;f38ba8;a6e3a1;f9e2af;89b4fa;f5c2e7;94e2d5;cdd6f4";
   boot.loader.limine.style.graphicalTerminal.brightPalette = "585b70;f38ba8;a6e3a1;f9e2af;89b4fa;f5c2e7;94e2d5;cdd6f4";
-  boot.loader.limine.style.graphicalTerminal.foreground = "1e1e2e";
-  boot.loader.limine.style.graphicalTerminal.background = "cdd6f4";
-  boot.loader.limine.style.graphicalTerminal.brightForeground = "cdd6f4";
-  boot.loader.limine.style.graphicalTerminal.brightBackground = "585b70";
 }
